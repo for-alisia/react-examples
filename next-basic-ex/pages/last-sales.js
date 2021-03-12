6b0564783640dbd39337fd95a813b2e0909eef88
for-alisia <for-alisia@yandex.ru>
2021-03-12 17:43:02 +0500

    Added SWR fetching

--- next-basic-ex/pages/last-sales.js
+++ next-basic-ex/pages/last-sales.js
@@ -1,45 +1,77 @@
 import { useEffect, useState } from 'react';
-const LastSales = () => {
-  const [sales, setSales] = useState();
-  const [isLoading, setIsLoading] = useState(false);
+import useSWR from 'swr';
+const LastSales = (props) => {
+  const [sales, setSales] = useState(props.sales);
+  // const [isLoading, setIsLoading] = useState(false);
+
+  const { data, error } = useSWR(
+    'https://testproject-for-dp-default-rtdb.firebaseio.com/sales.json'
+  );
 
   useEffect(() => {
-    setIsLoading(true);
-    fetch('https://testproject-for-dp-default-rtdb.firebaseio.com/sales.json')
-      .then((res) => res.json())
-      .then((data) => {
-        const transformedData = [];
+    if (data) {
+      const transformedData = [];
 
-        for (const key in data) {
-          transformedData.push({ id: key, username: data[key].username, volume: data[key].volume });
-        }
+      for (const key in data) {
+        transformedData.push({ id: key, username: data[key].username, volume: data[key].volume });
+      }
 
-        // @ts-ignore
-        setSales(transformedData);
-        setIsLoading(false);
-      });
-  }, []);
+      // @ts-ignore
+      setSales(transformedData);
+    }
+  }, [data]);
 
-  if (isLoading) {
-    return <p>Loading...</p>;
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   fetch('https://testproject-for-dp-default-rtdb.firebaseio.com/sales.json')
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       const transformedData = [];
+
+  //       for (const key in data) {
+  //         transformedData.push({ id: key, username: data[key].username, volume: data[key].volume });
+  //       }
+
+  //       // @ts-ignore
+  //       setSales(transformedData);
+  //       setIsLoading(false);
+  //     });
+  // }, []);
+
+  if (error) {
+    return <p>Failed to load!</p>;
   }
 
-  if (!sales) {
-    return <p>No data</p>;
+  if (!data && !sales) {
+    return <p>Loading...</p>;
   }
   return (
     <div>
       <h1>Last Sales</h1>
       <ul>
-        {sales &&
+        {
+          // @ts-ignore
           sales.map((sale) => (
             <li key={sale.id}>
               {sale.username} - {sale.volume}$
             </li>
-          ))}
+          ))
+        }
       </ul>
     </div>
   );
 };
+
+export async function getStaticProps() {
+  const response = await fetch('https://testproject-for-dp-default-rtdb.firebaseio.com/sales.json');
+  const data = await response.json();
+  const transformedData = [];
+
+  for (const key in data) {
+    transformedData.push({ id: key, username: data[key].username, volume: data[key].volume });
+  }
+
+  return { props: { sales: transformedData }, revalidate: 10 };
+}
 
 export default LastSales;
