3e1882378bcbd24cd20d4f0999aa4bc0f8e3ebd5
for-alisia <for-alisia@yandex.ru>
2021-03-23 11:40:30 +0300

    Changed todo-app

--- mobx-ex/src/todo.ts
+++ mobx-ex/src/todo.ts
@@ -1,11 +1,48 @@
-import { observable, action, makeObservable, autorun } from 'mobx';
+import { observable, action, makeObservable, computed, reaction, when } from 'mobx';
 
 type TTodoStatus = 'completed' | 'active';
 
 class Todo {
+  @observable
   status: TTodoStatus = 'active';
 
-  constructor(public id: string, public content: string) {}
+  @observable
+  content: string;
+
+  id: string;
+
+  disposer: () => void;
+
+  constructor(id: string, content: string) {
+    makeObservable(this);
+
+    this.content = content;
+    this.id = id;
+
+    this.disposer = reaction(
+      () => this.status,
+      () =>
+        console.log(`Item ${this.id} has changed: status: ${this.status}, content: ${this.content}`)
+    );
+  }
+
+  @action
+  changeContent(content: string) {
+    this.content = content;
+  }
+
+  @action
+  changeStatus(status: TTodoStatus) {
+    if (this.status === status) {
+      console.log(`Item is already ${status}`);
+      return;
+    }
+    this.status = status;
+  }
+
+  dispose() {
+    this.disposer();
+  }
 }
 
 class TodoList {
@@ -14,23 +51,43 @@ class TodoList {
 
   constructor() {
     makeObservable(this);
+
+    reaction(
+      () => this.items.length,
+      () =>
+        console.log(
+          `Total items: ${this.items.length}, Completed: ${this.completedTodos.length}, active: ${this.activeTodos.length}`
+        )
+    );
+
+    when(
+      () => this.items.length > 0 && this.items.every((item) => item.status === 'completed'),
+      () => console.log('All items were completed!')
+    );
+  }
+
+  findItem(id: string) {
+    return this.items.find((item) => item.id === id);
   }
 
   @action
-  addTodo(item: Todo) {
-    console.log('ACTION: Adding new todo to the list');
-    this.items.push(item);
+  addTodo(id: string, content: string) {
+    this.items.push(new Todo(id, content));
   }
 
   @action
   removeItem(id: string) {
-    console.log('ACTION: removing item');
+    const todoToDelete = this.findItem(id);
+    if (!todoToDelete) {
+      console.log('No Item With This ID was found');
+      return;
+    }
+    todoToDelete.dispose();
     this.items = this.items.filter((item) => item.id !== id);
   }
 
   @action
-  completeItem(id: string) {
-    console.log('ACTION: Completing todo');
+  changeItemStatus(id: string, status: TTodoStatus) {
     const todo = this.items.find((item) => item.id === id);
 
     if (!todo) {
@@ -38,29 +95,66 @@ class TodoList {
       return;
     }
 
-    if (todo.status === 'completed') {
-      console.log('Todo is already completed!');
+    todo.changeStatus(status);
+  }
+
+  @action
+  updateItemContent(id: string, content: string) {
+    const todo = this.items.find((item) => item.id === id);
+
+    if (!todo) {
+      console.log('Item with this id was not found');
       return;
     }
 
-    this.items = this.items.filter((item) => item.id !== id);
-    todo.status = 'completed';
-    this.items.push(todo);
+    todo.changeContent(content);
+  }
+
+  @computed
+  get activeTodos() {
+    return this.items.filter((item) => item.status === 'active');
+  }
+
+  @computed
+  get completedTodos() {
+    return this.items.filter((item) => item.status === 'completed');
   }
 }
 
 const todoList = new TodoList();
 
-autorun(() => {
-  console.log('TodoList:');
-  if (todoList.items.length === 0) {
-    console.log('No items inside yet');
-  }
-  todoList.items.map((item) => console.log(item));
-  console.log('------');
-});
+// autorun(() => {
+//   console.log('TodoList:');
+//   if (todoList.items.length === 0) {
+//     console.log('No items inside yet');
+//   }
+//   todoList.items.map((item) =>
+//     console.log(`Item with id ${item.id}: ${item.content}, status: ${item.status}`)
+//   );
 
-todoList.addTodo(new Todo('td1', 'first todo'));
-todoList.addTodo(new Todo('td2', 'second todo'));
-todoList.completeItem('td1');
+//   console.log('ACTIVE:');
+//   if (todoList.activeTodos.length === 0) {
+//     console.log('No active todo was found');
+//   }
+//   todoList.activeTodos.map((item) =>
+//     console.log(`Item with id ${item.id}: ${item.content}, status: ${item.status}`)
+//   );
+
+//   console.log('COMPLETED:');
+//   if (todoList.completedTodos.length === 0) {
+//     console.log('No completed todo was found');
+//   }
+//   todoList.completedTodos.map((item) =>
+//     console.log(`Item with id ${item.id}: ${item.content}, status: ${item.status}`)
+//   );
+
+//   console.log('------');
+// });
+
+todoList.addTodo('td1', 'first todo');
+todoList.addTodo('td2', 'second todo');
+todoList.changeItemStatus('td1', 'completed');
+todoList.changeItemStatus('td1', 'active');
+todoList.updateItemContent('td2', 'Updated content');
 todoList.removeItem('td2');
+todoList.changeItemStatus('td1', 'completed');
